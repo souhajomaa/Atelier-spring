4dd6c552da6e1e4dd60b0111d355d46701928e68
souhajomaa <souhajomaa1412@gmail.com>
2025-12-07 17:15:25 +0100

    Scheduled

--- src/main/java/com/example/spring/tpfoyer/services/ChambreServiceImpl.java
+++ src/main/java/com/example/spring/tpfoyer/services/ChambreServiceImpl.java
@@ -4,15 +4,14 @@ package com.example.spring.tpfoyer.services;
 import com.example.spring.tpfoyer.entity.Chambre;
 import com.example.spring.tpfoyer.repository.ChambreRepository;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Slf4j
+
 @Service
 @AllArgsConstructor
 public class ChambreServiceImpl implements ChambreService{
+    @Autowired
     ChambreRepository chambreRepository;
     public List<Chambre> retrieveAllChambres() {
         return chambreRepository.findAll();
