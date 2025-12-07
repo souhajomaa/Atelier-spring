05fba97ddaef86027cdc275c585970b38ec0f808
souhajomaa <souhajomaa1412@gmail.com>
2025-12-07 17:16:33 +0100

    Scheduled

--- src/main/java/com/example/spring/tpfoyer/services/ChambreServiceImpl.java
+++ src/main/java/com/example/spring/tpfoyer/services/ChambreServiceImpl.java
@@ -4,14 +4,16 @@ package com.example.spring.tpfoyer.services;
 import com.example.spring.tpfoyer.entity.Chambre;
 import com.example.spring.tpfoyer.repository.ChambreRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class ChambreServiceImpl implements ChambreService{
-    @Autowired
     ChambreRepository chambreRepository;
     public List<Chambre> retrieveAllChambres() {
         return chambreRepository.findAll();
@@ -28,5 +30,11 @@ public class ChambreServiceImpl implements ChambreService{
     public Chambre modifyChambre(Chambre chambre) {
         return chambreRepository.save(chambre);
     }
-
+    //Scheduled
+    @Scheduled(cron = "0/15 * * * * *")
+    public void pourcentageChambreParTypeChambre() {
+        List<Chambre> chambreList = chambreRepository.findAll();
+        int total = chambreList.size();
+        log.info("nombre de chambres:"+ total);
+    }
 }
